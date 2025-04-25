84c652a7ef5432d6f469302d640baee10c773674
Denys Kibalenko <dennieking87@gmail.com>
2025-04-25 12:34:47 +0200

    Fix: add `nullable=False` to `Author.name` field

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Author(Base):
     __tablename__ = "authors"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True)
+    name = Column(String, nullable=False, unique=True)
     bio = Column(String)
     books = relationship("Book", back_populates="author")
 
