17fdf28c3896eec0b8bfc5a582ab6b786ed96b35
Denys Kibalenko <dennieking87@gmail.com>
2025-04-27 11:42:46 +0200

    Feat: add `get_author_list`, `get_single_author` & `create_author` endpoints to main.py

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query, Depends
+from sqlalchemy.orm import Session
+
+import schemas,crud
+from database import get_db
 	
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+@app.get("/authors/", response_model=schemas.PaginatedAuthors)
+def get_author_list(
+    db: Session = Depends(get_db),
+    skip: int = Query(0),
+    limit: int = Query(10)
+):
+    authors = crud.get_authors(db, skip, limit)
+    return schemas.PaginatedAuthors(
+        items=authors,
+        skip=skip,
+        limit=limit
+    )
+
+
+@app.get("/authors/{author_id}", response_model=schemas.Author)
+def get_single_author(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.get_author(db=db, author_id=author_id)
+
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author not found.")
+
+    return db_author
+
+
+@app.post("/authors/", response_model=schemas.Author)
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    db_author = crud.get_author_by_name(db=db, name=author.name)
+
+    if db_author is not None:
+        raise HTTPException(status_code=400, detail="Author already exists.")
+
+    return crud.create_author(db=db, author=author)
